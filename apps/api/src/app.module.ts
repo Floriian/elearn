43371ea8f868f5c8aab2f7eb22e1661bd4b99cd4
Floriian <lovasifloriian@outlook.hu>
2023-09-18 21:47:37 +0200

    api: user module & typeorm migarions.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,12 +3,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '@/auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     AuthModule,
     TypeOrmModule.forRootAsync(typeormConfig),
     ConfigModule.forRoot(),
+    UserModule,
   ],
   controllers: [],
   providers: [],
