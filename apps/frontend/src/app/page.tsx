d3489836d72cc923bbb5e5b838f3b4d45df1462f
Floriian <lovasifloriian@outlook.hu>
2023-09-21 21:33:32 +0200

    remove nextjs app.

--- apps/frontend/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import { AuthModal } from '@/features'
-
-export default function Home() {
-  return (
-    <AuthModal></AuthModal>
-  )
-}
