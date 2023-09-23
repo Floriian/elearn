386c1c23fdaffa489752f0dd0caa91e4e9683bc9
Floriian <lovasifloriian@outlook.hu>
2023-09-23 18:20:11 +0200

    fe: add title to newslist.

--- apps/client/src/features/news/public/components/NewsList.tsx
+++ apps/client/src/features/news/public/components/NewsList.tsx
@@ -1,8 +1,10 @@
+import { Title } from "@/components";
 import { NewsCard, useGetAllNewsQuery } from "@/features"
 export function NewsList() {
     const { data: newsList } = useGetAllNewsQuery();
     return (
         <>
+            <Title level={2}>News</Title>
             {newsList?.map((news) => (
                 <NewsCard news={news} key={news.id} />
             ))}
