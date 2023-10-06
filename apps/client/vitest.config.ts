794e2b82eddb5a9f26e4bc7688125542b1b6f3c4
Floriian <lovasifloriian@outlook.hu>
2023-10-06 18:50:25 +0200

    create vitest config.

---
+++ apps/client/vitest.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vitest/config";
+
+import { fileURLToPath } from "node:url";
+
+const root = fileURLToPath(new URL("./src", import.meta.url));
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": root,
+    },
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    reporters: "verbose",
+    setupFiles: "./src/test/setup.ts",
+  },
+});
