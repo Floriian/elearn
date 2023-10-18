570d548558904bccd2a1a9ff2c91819906f58b2e
Floriian <lovasifloriian@outlook.hu>
2023-10-18 21:52:35 +0200

    another try.

--- apps/client/src/utils/axios/axios.instance.ts
+++ apps/client/src/utils/axios/axios.instance.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
-});
+export const axiosInstance = axios.create();
 
 axiosInstance.interceptors.request.use((config) => {
   console.log(config);
