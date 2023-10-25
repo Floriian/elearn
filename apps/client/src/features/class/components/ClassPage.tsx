5e70d3db1d2621871b5ec34180ebdd149e46357f
Floriian <lovasifloriian@outlook.hu>
2023-10-25 23:01:33 +0200

    search on classpage.

--- apps/client/src/features/class/components/ClassPage.tsx
+++ apps/client/src/features/class/components/ClassPage.tsx
@@ -1,16 +1,25 @@
-import { ClassList, useGetClassesQuery } from "@/features";
-import { Pagination, Space } from "antd";
-import { useState } from "react"
+import { Class, ClassList, useGetClassesQuery } from "@/features";
+import { SearchOutlined } from "@ant-design/icons";
+import { Input, Pagination, Space } from "antd";
+import { useEffect, useState } from "react"
 
 export function ClassPage() {
 
     const [currentPage, setCurrentPage] = useState<number>(1);
+    const [classes, setClasses] = useState<Class[]>();
+    const [searchText, setSearchText] = useState<string>('');
+
     const { data } = useGetClassesQuery({ page: currentPage })
 
 
     const handleOnChange = (page: number) => {
         setCurrentPage(page);
     }
+
+    useEffect(() => {
+        const filteredClasses = data ? data.data.filter((value) => value.title.includes(searchText)) : []
+        setClasses(filteredClasses)
+    }, [searchText, data])
 
     return (
         <div style={{
@@ -19,14 +28,21 @@ export function ClassPage() {
             flexDirection: 'column',
             justifyContent: 'space-between'
         }}>
+            <Input
+                addonBefore={<SearchOutlined />}
+                placeholder="Search..."
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+            />
+
             <Space style={{ marginLeft: '5rem' }}>
-                {data && (<ClassList data={data?.data} />)}
+                {classes && (<ClassList data={classes} />)}
             </Space>
 
             <Space direction="horizontal" style={{ marginLeft: '5rem', display: 'flex', justifyContent: 'center' }}>
                 <Pagination
                     defaultPageSize={8}
-                    total={data && data.count}
+                    total={data?.count}
                     onChange={handleOnChange}
                     defaultCurrent={1}
                 />
